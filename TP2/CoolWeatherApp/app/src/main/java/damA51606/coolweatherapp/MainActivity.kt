4afc885bdcd293a08bd5f0caa80f7e94a59a8974
z1lyb <zisajoia@gmail.com>
2026-04-08 20:44:27 +0100

    portrait layout beginning

--- TP2/CoolWeatherApp/app/src/main/java/damA51606/coolweatherapp/MainActivity.kt
+++ TP2/CoolWeatherApp/app/src/main/java/damA51606/coolweatherapp/MainActivity.kt
@@ -1,5 +1,6 @@
 package damA51606.coolweatherapp
 
+import android.content.res.Configuration
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
@@ -10,6 +11,27 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
+
+        val day = true
+
+        when (resources.configuration.orientation) {
+            Configuration.ORIENTATION_PORTRAIT -> {
+                if (day) {
+                    setTheme(R.style.Theme_Day)
+                } else {
+                    setTheme (R.style.Theme_Night)
+                }
+            }
+            Configuration. ORIENTATION_LANDSCAPE -> {
+                if (day) {
+                    setTheme (R.style.Theme_Day_Land)
+                } else {
+                    setTheme (R.style.Theme_Night_Land)
+                }
+            }
+        }
+
+
         setContentView(R.layout.activity_main)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
