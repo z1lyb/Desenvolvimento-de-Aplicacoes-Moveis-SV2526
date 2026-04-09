61c52467d1548f83c91dfbda69941cf85828834f
z1lyb <zisajoia@gmail.com>
2026-04-09 15:09:42 +0100

    weather api call function

--- TP2/CoolWeatherApp/app/src/main/java/damA51606/coolweatherapp/MainActivity.kt
+++ TP2/CoolWeatherApp/app/src/main/java/damA51606/coolweatherapp/MainActivity.kt
@@ -6,6 +6,9 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import java.io.InputStreamReader
+import java.net.URL
+import com.google.gson.Gson
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -37,6 +40,21 @@ class MainActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+    }
+
+    private fun WeatherAPI_Call(lat:Float, long:Float) : WeatherData {
+        val reqString = buildString {
+            append("https://api.open-meteo.com/v1/forecast?")
+            append("latitude=${lat}&longitude=${long}&")
+            append("current_weather=true&")
+            append("hourly=temperature_2m,weathercode,pressure_msl,windspeed_10m")
+        }
+        val str = reqString.toString()
+        val url = URL(str)
+        url.openStream().use {
+            val request = Gson().fromJson(InputStreamReader(it,"UTF-8"), WeatherData::class.java)
+            return request
         }
     }
 }
