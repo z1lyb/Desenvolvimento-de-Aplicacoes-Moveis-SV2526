a725ef3e17ac72c2693fda494bc8fb819683ec22
z1lyb <zisajoia@gmail.com>
2026-04-19 17:16:03 +0100

    CoolJetpackWeatherApp created, data layer implemented

---
+++ TP3/CoolJetpackWeatherApp/app/build.gradle.kts
@@ -0,0 +1,56 @@
+plugins {
+    alias(libs.plugins.android.application)
+    kotlin("plugin.serialization") version "2.2.21"
+}
+
+android {
+    namespace = "dam_a51606.cooljetpackweatherapp"
+    compileSdk {
+        version = release(36) {
+            minorApiLevel = 1
+        }
+    }
+
+    defaultConfig {
+        applicationId = "dam_a51606.cooljetpackweatherapp"
+        minSdk = 24
+        targetSdk = 36
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
+    }
+}
+
+dependencies {
+    implementation(libs.androidx.core.ktx)
+    implementation(libs.androidx.appcompat)
+    implementation(libs.material)
+    implementation(libs.androidx.activity)
+    implementation(libs.androidx.constraintlayout)
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.androidx.junit)
+    androidTestImplementation(libs.androidx.espresso.core)
+    implementation("com.google.code.gson:gson:2.8.9")
+
+    // Ktor Client (for WeatherAPIClient)
+    val ktor_version = "3.4.2"
+    implementation("io.ktor:ktor-client-core:${ktor_version}")
+    implementation("io.ktor:ktor-client-android:${ktor_version}")
+    implementation("io.ktor:ktor-client-content-negotiation:${ktor_version}")
+    implementation("io.ktor:ktor-serialization-kotlinx-json:${ktor_version}")
+}
---
+++ TP3/CoolJetpackWeatherApp/app/src/main/java/dam_a51606/cooljetpackweatherapp/data/WeatherAPIClient.kt
@@ -0,0 +1,37 @@
+package dam_a51606.cooljetpackweatherapp.data
+
+import io.ktor.client.*
+import io.ktor.client.call.*
+import io.ktor.client.plugins.contentnegotiation.*
+import io.ktor.client.request.*
+import io.ktor.serialization.kotlinx.json.*
+import kotlinx.serialization.json.Json
+
+object WeatherAPIClient {
+    private val client = HttpClient {
+        install(ContentNegotiation)  {
+            json(Json {
+                prettyPrint = true
+                isLenient = true
+                ignoreUnknownKeys = true
+            }) // Ignores extra JSON fields
+        }
+    }
+
+    suspend fun getWeather (lat: Float, lon: Float): WeatherData? {
+        val reqString = buildString {
+            append("https://api.open-meteo.com/v1/forecast?")
+            append("latitude=${lat}&longitude=${lon}&")
+            append("current_weather=true&")
+            append("hourly=temperature_2m,weathercode,pressure_msl,windspeed_10m")
+        }
+        System.out.println("Getting URL: $reqString")
+        return try {
+            client.get(reqString).body() // Parses JSON into WeatherData
+        } catch(e: Exception) {
+            e.printStackTrace()
+            null
+        }
+    }
+
+}
